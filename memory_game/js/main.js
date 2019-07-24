a46ff1fcd0fd0338e9a777cb8429fed85eb601ab
BuildTools <unconfigured@null.spigotmc.org>
2019-07-24 01:40:24 -0400

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -22,25 +22,39 @@ var cards = [
 
 var cardsInPlay = [];
 
+
 function checkForMatch(){
 	if (cardsInPlay[0] === cardsInPlay[1]) {
-	  console.log("You found a match!");
+	  alert("You found a match!");
 	} else {
-	  console.log("Sorry, try again.");
+	  alert("Sorry, try again.");
 	}
 }
 
-function flipCard(cardId){
+function flipCard(){
+	var cardId = this.getAttribute('data-id');
 	console.log("User flipped " + cards[cardId].rank);
 	console.log(cards[cardId].suit);
 	console.log(cards[cardId].cardImage);
 	cardsInPlay.push(cards[cardId].rank);
+
+	this.setAttribute('src', cards[cardId].cardImage);
 
 	if (cardsInPlay.length === 2){
 		checkForMatch();
 	}
 }
 
-flipCard(0);
-flipCard(2);
+function createBoard(){
+	for(var i = 0; i < cards.length; i++){
+		var cardElement = document.createElement('img');
+		cardElement.setAttribute('src', "images/back.png");
+		cardElement.setAttribute('data-id', i);
+		cardElement.addEventListener('click', flipCard);
+		document.getElementById('game-board').appendChild(cardElement);
+	}
+}
+
+
+createBoard();
 
